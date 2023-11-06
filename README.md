67621445f68b053cd34a341d3de6a87c7776989d
Tyler Johnston <johnstontyler@protonmail.com>
2023-11-06 12:02:06 -0700

    i now understand the reqs better, here is a basic main func

--- README.md
+++ README.md
@@ -1,3 +1,105 @@
-How to connect to EC2 instance:
+0. How to connect to EC2 instance:
 - chmod 400 /path/to/your-key.pem
 - ssh -i /path/to/your-key.pem ec2-user@your-ec2-public-dns
+
+1. <b>producer.jar</b>:
+- the producer.jar is responsible for generating widget-related requests. These requests are data structures that instruct how a widget should be created, updated, or deleted.
+- Once a request is generated, producer.jar stores this request in an S3 bucket as an object. Each object in S3 has a unique key, which helps identify and retrieve it.
+- After storing the request in S3, producer.jar sends a message to an Amazon Simple Queue Service (SQS) queue. This message doesn't contain the full request data. Instead, it contains information (like the S3 object's key) that points to the request stored in the S3 bucket. Essentially, the message in SQS acts as a pointer or reference to the actual request data in S3.
+
+2. <b>consumer.py</b>:
+- This script runs in a loop, constantly checking the SQS queue for new messages. 
+- When it finds a message in the queue, it uses the information in that message to retrieve the corresponding request from the S3 bucket. The RetrieveRequest function handles this by fetching the object (request data) from the S3 bucket using the provided key.
+- Once the request is retrieved, the consumer.py script processes it. Depending on the request's instructions, it can:
+    - Create a new widget
+    - Update an existing widget
+    - Delete a widget
+- The actual widget data, based on the request's content, can be stored in a different S3 bucket or a DynamoDB table, depending on the specified storage strategy.
+- After the request has been processed, the script deletes the message from the SQS queue using the DeleteFromQueue function. It also deletes the request from the S3 bucket using the DeleteRequest function to ensure it doesn't process the same request again.
+
+
+## how to use producer.jar to populate SQS:
+ava -jar producer.jar -rq https://sqs.us-east-1.amazonaws.com/...
+
+<!-- aws s3 cp s3://usu-cs5260-tylerj-requests/instructor-producer.jar 
+
+java -jar consumer.jar --request-bucket=usu-cs5260-tylerj-requests --widget-bucket=usu-cs5260-tylerj-web
+
+java -jar producer.jar --request-bucket=usu-cs5260-tylerj-requests
+java -jar producer.jar --sqs-queue-name=cs5260-requests
+
+aws s3 cp s3://usu-cs5260-tylerj-dist/instructor-consumer.jar ./ -->
+
+
+---
+
+Define a New Function to Send to SQS:
+
+Before processing the widget request, we'll send the request to the SQS queue. Here's a simple function to achieve that:
+
+python
+
+def SendToSQS(queueURL, messageBody):
+    try:
+        SQS_CLIENT.send_message(QueueUrl=queueURL, MessageBody=json.dumps(messageBody))
+        logging.info(f"Request sent to SQS: {messageBody}")
+    except Exception as e:
+        logging.error(f"Error sending message to SQS: {e}")
+
+Modify the RetrieveRequest Function:
+
+After retrieving the request from the S3 bucket, send it to the SQS queue:
+
+python
+
+def RetrieveRequest(source, queueURL):
+    try:
+        # ... (rest of the code)
+
+        # After successfully retrieving the request, send it to SQS
+        SendToSQS(queueURL, request)
+
+        logging.info(f"Request with key {key} retrieved.")
+        return request, key
+    except Exception as e:
+        logging.error(f"Error retrieving request: {e}")
+    return None, None
+
+Modify the main Function:
+
+Change the while loop to first fetch from the queue and then process the request:
+
+python
+
+def main(source, destination, storage, queueURL):
+    while True:
+        # Instead of fetching from the S3 bucket, fetch from the SQS queue:
+        messages = FetchFromQueue(queueURL)
+        for message in messages:
+            request = json.loads(message['Body'])
+            if request:
+                ProcessRequest(request, destination, storage)
+                # No need to delete from S3 here as you're processing from SQS
+                DeleteFromQueue(queueURL, message['ReceiptHandle'])
+        if not messages:
+            time.sleep(.1)
+
+The line request, key = RetrieveRequest(source, body) is removed from the main loop because now you're directly fetching from the SQS queue, and the request is the message body.
+
+Ensure Command-Line Argument for SQS URL is Required:
+
+Make the queue-url command-line argument mandatory:
+
+python
+
+parser.add_argument('--queue-url', required=True, help='URL of the SQS queue')
+
+Run the Consumer:
+
+You'd run the consumer specifying the SQS queue URL:
+
+bash
+
+    python3 consumer.py --request-source usu-cs5260-tylerj-requests --request-destination usu-cs5260-tylerj-web --storage-strategy s3 --queue-url YOUR_SQS_QUEUE_URL
+
+By making these changes, the Consumer will add the widget requests to the SQS queue after they're retrieved from the S3 bucket. Then, the main loop will process these requests directly from the SQS queue, allowing for efficient and scalable processing.
--- consumer.py
+++ consumer.py
@@ -16,7 +16,14 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s'
 )
 
-def RetrieveRequest(source):
+def SendToSQS(queueURL, messageBody):
+    try:
+        SQS_CLIENT.send_message(QueueUrl=queueURL, MessageBody=json.dumps(messageBody))
+        logging.info(f"Request sent to SQS: {messageBody}")
+    except Exception as e:
+        logging.error(f"Error sending message to SQS: {e}")
+
+def RetrieveRequest(source, queueURL):
     try:
         # the request data's key is numeric with the same number of digits each time; thus, grabbing the first element should be the smallest key
         response = S3_CLIENT.list_objects_v2(Bucket=source, MaxKeys=1)
@@ -25,6 +32,7 @@ def RetrieveRequest(source):
             # obtain the actual content of the object
             myObject = S3_CLIENT.get_object(Bucket=source, Key=key)
             request = json.loads(myObject['Body'].read().decode('utf-8'))
+            SendToSQS(queueURL, request)
             logging.info(f"Request with key {key} retrieved.")
             return request, key
     except Exception as e:
@@ -134,44 +142,80 @@ def GetDynamoAttribute(value):
     elif isinstance(value, dict):
         return {"M": {key: GetDynamoAttribute(val) for key, val in value.items()}}
 
-def DeleteRequest(key, source):
+def DeleteFromStorage(key, source):
     try:
         S3_CLIENT.delete_object(Bucket=source, Key=key)
         logging.info(f"Request with key {key} deleted from {source}")
     except Exception as e:
         logging.error(f"Error deleting request with key {key}: {e}")
 
+# needs to retrieve widget reqeusts from SQS rather than S3 bucket if specified
 def main(source, destination, storage, queueURL):
     while True:
-        messages = FetchFromQueue(queueURL)
-        for message in messages:
-            body = json.loads(message['Body'])
-            request, key = RetrieveRequest(source, body)  # Assumes body contains required info to retrieve S3 object.
+        if storage:
+            request, key = RetrieveRequest(source)
             if request:
                 ProcessRequest(request, destination, storage)
-                DeleteRequest(key, source)
+                DeleteFromStorage(key, source)
+            else:
+                time.sleep(.1)
+        elif queueURL:
+            requests = FetchFromQueue(queueURL)
+            for request in requests:
+                ProcessRequest(request, destination, storage)
                 DeleteFromQueue(queueURL, message['ReceiptHandle'])
-        if not messages:
-            time.sleep(.1)
+
+            
+
+
+# def main(source, destination, storage, queueURL):
+#     while True:
+#         messages = FetchFromQueue(queueURL)
+#         for message in messages:
+#             body = json.loads(message['Body'])
+#             request, key = RetrieveRequest(source, body)  # Assumes body contains required info to retrieve S3 object.
+#             if request:
+#                 ProcessRequest(request, destination, storage)
+#                 DeleteRequest(key, source) # need to keep this here to ensure the 'all previous implementation is in tact' requirement
+#                 DeleteFromQueue(queueURL, message['ReceiptHandle'])
+#         if not messages:
+#             time.sleep(.1)
+
+# def main(source, destination, storage):
+#     while True:
+#         request, key = RetrieveRequest(source)
+#         if request:
+#             ProcessRequest(request, destination, storage)
+#             DeleteRequest(key, source)
+#         else:
+#             time.sleep(.1)
 
 
 # this will only run if consumer.py is run directly, not being imported
 # i needed to place the command-line args here to let my unit test program work
 if __name__ == "__main__":
     # Command-line arguments setup 
-    # EXAMPLE: python3 consumer.py --request-source usu-cs5260-tylerj-requests --request-destination usu-cs5260-tylerj-web --storage-strategy s3
-    # python3 consumer.py --request-source usu-cs5260-tylerj-requests --request-destination widgets --storage-strategy dynamodb
+    # EXAMPLES:
+    # python3 consumer.py --request-source usu-cs5260-tylerj-requests --request-destination widgets --storage-strategy s3
+    # python3 consumer.py --queue-url https://sqs.us-west-2.amazonaws.com/123456789012/myqueue --request-destination widgets --storage-strategy dynamodb
+
     parser = argparse.ArgumentParser(description='Consumer program to process requests to create, update, or delete widgets')
-    parser.add_argument('--request-source', required=False, help='S3 bucket where the requests are fetched from')
-    parser.add_argument('--request-destination', required=False, help='Choose where to store the widgets')
-    parser.add_argument('--storage-strategy', required=False, choices=['s3', 'dynamodb'], help='Choose \'s3\' to store widgets in a bucket or \'dynamodb\' to store widgets in a dynamodb table')
-    parser.add_argument('--queue-url', required=False, help='URL of the SQS queue')
+    parser.add_argument('--storage-strategy', required=True, choices=['s3', 'dynamodb'], help='Choose \'s3\' to store widgets in a bucket or \'dynamodb\' to store widgets in a dynamodb table')
+    parser.add_argument('--request-destination', required=True, help='Choose where to store the widgets')
+
+    # initialize a mutually exclusive group
+    group = parser.add_mutually_exclusive_group(required=True)
+
+    # adding request-source and queue-url ensures one of them is required but not both
+    group.add_argument('--request-source', help='S3 bucket where the requests are fetched from')
+    group.add_argument('--queue-url', help='URL of the SQS queue')
 
     args = parser.parse_args()
 
-    # Define command-line argument constants
+    # define command-line argument constants
     REQUEST_SOURCE = args.request_source
     REQUEST_DESTINATION = args.request_destination
     STORAGE_STRATEGY = args.storage_strategy
     QUEUE_URL = args.queue_url
+
     main(REQUEST_SOURCE, REQUEST_DESTINATION, STORAGE_STRATEGY, QUEUE_URL)
